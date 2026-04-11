2df895ab217e4bfbfaac4662832bc027752c76e6
Matěj Přikryl <matej.prikryl@tul.cz>
2026-04-11 18:05:36 +0200

    Added Business Logic for finding Extremes

--- backend/src/main/java/cz/tul/stin/backend/config/StartupRunner.java
+++ backend/src/main/java/cz/tul/stin/backend/config/StartupRunner.java
@@ -1,5 +1,8 @@
 package cz.tul.stin.backend.config;
 
+import cz.tul.stin.backend.model.CurrencyExtremes;
+import cz.tul.stin.backend.service.MockRateClient;
+import cz.tul.stin.backend.service.RateCalculatorService;
 import cz.tul.stin.backend.service.RateClient;
 import org.jspecify.annotations.NonNull;
 import org.springframework.boot.CommandLineRunner;
@@ -11,16 +14,18 @@ import java.util.Map;
 @Component
 @Profile("dev")
 public class StartupRunner implements CommandLineRunner {
-    private final RateClient rateClient;
+    private RateCalculatorService calculator;
 
-    public StartupRunner(RateClient rateclient) {
-        this.rateClient = rateclient;
+    public StartupRunner(RateCalculatorService calculator) {
+        this.calculator = calculator;
     }
 
     @Override
     public void run(String @NonNull ... args) throws Exception {
-        var response = rateClient.getLiveRates("EUR");
-        Map<String, Double> quotes = response.getQuotes();
-        System.out.println(quotes.get("EURCZK"));
+        var client = new MockRateClient();
+        calculator = new RateCalculatorService(client);
+        CurrencyExtremes extremes = calculator.getExtremes("EUR");
+        System.out.printf("Strongest: %s (%f)%n", extremes.strongest().getKey() ,extremes.strongest().getValue());
+        System.out.printf("Weakest: %s (%f)%n", extremes.weakest().getKey() ,extremes.weakest().getValue());
     }
 }
