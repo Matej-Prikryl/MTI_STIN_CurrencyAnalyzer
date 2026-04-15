e205fa62363a9fa6049a484063faebeaa05fc2eb
Matěj Přikryl <matej.prikryl@tul.cz>
2026-04-15 22:09:19 +0200

    Currency Filtering (#15)
    * Added Currency Filtering
    
    * Tests

--- backend/src/main/java/cz/tul/stin/backend/config/StartupRunner.java
+++ backend/src/main/java/cz/tul/stin/backend/config/StartupRunner.java
@@ -12,6 +12,8 @@ import org.springframework.stereotype.Component;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.ZoneId;
+import java.util.HashSet;
+import java.util.Set;
 
 @Component
 @Profile("dev")
@@ -25,11 +27,19 @@ public class StartupRunner implements CommandLineRunner {
     @Override
     public void run(String @NonNull ... args) throws Exception {
         System.out.println("Starting...");
+
         var client = new MockRateClient();
         Clock clock = Clock.fixed(Instant.parse("2011-01-01T10:00:00Z"), ZoneId.of("UTC"));
         var repository = new CurrencyRepository(client, clock);
         calculator = new RateCalculatorService(repository);
-        CurrencyInfo info = calculator.getCurrencyInfo("EUR", "2010-03-01", "2010-03-02");
+
+        Set<String> targetCurrencies = new HashSet<>();
+        targetCurrencies.add("EURUSD");
+        targetCurrencies.add("EUREUR");
+        targetCurrencies.add("EURGBP");
+        targetCurrencies.add("EURCZK");
+
+        CurrencyInfo info = calculator.getCurrencyInfo("EUR", "2010-03-01", "2010-03-02",  targetCurrencies);
         System.out.printf("Strongest: %s (%f)%n", info.extremes().strongest().getKey() ,info.extremes().strongest().getValue());
         System.out.printf("Weakest: %s (%f)%n", info.extremes().weakest().getKey() ,info.extremes().weakest().getValue());
         System.out.println("Averages:");
