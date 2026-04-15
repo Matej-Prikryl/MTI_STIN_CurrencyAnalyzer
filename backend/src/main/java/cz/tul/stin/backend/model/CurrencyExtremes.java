5f9ba4844620fe28cd408f10e70572472c1bb346
Matěj Přikryl <matej.prikryl@tul.cz>
2026-04-15 10:00:25 +0200

    Revert "Caching to Local Storage (#8)" (#9)
    This reverts commit 9a3ca26f29f25da0060ab642088179852da18d1f.

--- backend/src/main/java/cz/tul/stin/backend/model/CurrencyExtremes.java
+++
@@ -1,8 +0,0 @@
-package cz.tul.stin.backend.model;
-
-import java.util.Map;
-
-public record CurrencyExtremes(
-        Map.Entry<String, Double> strongest,
-        Map.Entry<String, Double> weakest
-) { }
--- backend/src/main/java/cz/tul/stin/backend/service/RateClient.java
+++
@@ -1,7 +0,0 @@
-package cz.tul.stin.backend.service;
-
-import cz.tul.stin.backend.model.TimeframeRateResponse;
-
-public interface RateClient {
-    TimeframeRateResponse getTimeframeRates(String base);
-}
