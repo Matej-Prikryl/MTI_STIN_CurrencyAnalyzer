d27bc74f2679f79aaaf541acd4bfe7334aab7994
Matěj Přikryl <matej.prikryl@tul.cz>
2026-04-20 15:48:07 +0200

    FE communicates with BE

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,26 @@
+const calculateBtn = document.getElementById('calculate-btn');
+const ratesTable = document.getElementById('rates-table');
+
+calculateBtn.addEventListener('click', async () => {
+    const baseCurrency = document.getElementById('base-currency').value;
+    const startDate = document.getElementById('start-date').value;
+    const endDate = document.getElementById('end-date').value;
+
+    console.log(`Base Currency: ${baseCurrency}, Start Date: ${startDate}, End Date: ${endDate}`);
+
+    const url = `/api/rates/currencyinfo?base=${baseCurrency}&startDate=${startDate}&endDate=${endDate}&targetCurrencies=USD`;
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    } catch (error) {
+        console.error('Error fetching currency rates:', error);
+    }
+
+});
+
+
 document.addEventListener('DOMContentLoaded', () => {
 
     const dateConfig = {
