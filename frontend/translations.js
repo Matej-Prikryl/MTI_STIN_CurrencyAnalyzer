c041f47f1d3b771d8df5cd2a95b94f356338113f
Matěj Přikryl <matej.prikryl@tul.cz>
2026-04-21 22:34:31 +0200

    Translation for Log In

---
+++ frontend/translations.js
@@ -0,0 +1,52 @@
+const translations = {
+    "en": {
+        "app-title": "Currency Analyzer",
+        "base-currency": "Base Currency",
+        "start-date": "From",
+        "end-date": "To",
+        "calculate-btn": "Calculate",
+        "strongest-currency": "Strongest",
+        "weakest-currency": "Weakest",
+        "currency": "Currency",
+        "average-rate": "Average Rate",
+
+        "login-title": "Log In",
+        "login-username": "Username",
+        "login-password": "Password",
+        "login-button": "Log In"
+    },
+    "cs": {
+        "app-title": "Měnový Analyzér",
+        "base-currency": "Základní Měna",
+        "start-date": "Od",
+        "end-date": "Do",
+        "calculate-btn": "Vypočítat",
+        "strongest-currency": "Nejsilnější",
+        "weakest-currency": "Nejslabší",
+        "currency": "Měna",
+        "average-rate": "Průměrný Kurz",
+
+        "login-title": "Přihlášení",
+        "login-username": "Uživatelské jméno",
+        "login-password": "Heslo",
+        "login-button": "Přihlásit se"
+    }
+};
+
+let currentLang = 'cs'; //localStorage.getItem('lang') || 'cs';
+
+function changeLanguage(lang) {
+    currentLang = lang;
+    localStorage.setItem('lang', lang);
+    
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+        const key = element.getAttribute('data-i18n');
+        if (translations[lang][key]) {
+            element.textContent = translations[lang][key];
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    changeLanguage(currentLang);
+});
