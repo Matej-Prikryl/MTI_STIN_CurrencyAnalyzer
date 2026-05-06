34ad308df0e974f3753ca42d02a047f3a925a347
Matěj Přikryl <matej.prikryl@tul.cz>
2026-05-06 09:09:11 +0200

    Fix saving settings (#62)

--- backend/src/main/java/cz/tul/stin/backend/service/SettingsService.java
+++ backend/src/main/java/cz/tul/stin/backend/service/SettingsService.java
@@ -1,18 +1,19 @@
 package cz.tul.stin.backend.service;
 
+import java.io.File;
+
+import org.springframework.stereotype.Service;
+
 import cz.tul.stin.backend.model.Settings;
 import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
 import tools.jackson.databind.ObjectMapper;
-
-import java.io.File;
 
 @Service
 @Slf4j
 @NoArgsConstructor
 public class SettingsService {
-    private final String FILE_PATH = "settings.json";
+    private final String FILE_PATH = System.getenv("HOME") != null ? System.getenv("HOME") + "/settings.json" : "settings.json";
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     public Settings loadSettings() {
